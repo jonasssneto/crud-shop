76daf288c66a09f6d6944f3ab2bb2dc0ea1f6468
Jonas Neto <nogueiraneto.jonas@gmail.com>
2025-06-11 00:12:33 -0300

    feat: ajustes env

--- src/util/Database.java
+++ src/util/Database.java
@@ -4,10 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Database {
-    private static final String URL = "jdbc:mysql://admin:admin@localhost:3306/database";
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final String url = dotenv.get("DB_URL");
 
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL);
+        return DriverManager.getConnection(url);
     }
 }
